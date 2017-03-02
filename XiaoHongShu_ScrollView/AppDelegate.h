9a818da5d9223d35edbcd611d7654ded941d4bf6
Piao Piao <94313353@qq.com>
2017-03-02 14:57:56 +0800

    Initial Commit

---
+++ XiaoHongShu_ScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiaoHongShu_ScrollView
+//
+//  Created by Piao Piao on 2017/3/2.
+//  Copyright © 2017年 Piao Piao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
